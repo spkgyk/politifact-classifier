86b8db19588920d888d1a2e06a58e5b0176512f0
spkgyk <spkgyk@gmail.com>
2024-06-07 16:19:01 +0800

    reformat code

--- src/utils/random_forest.py
+++ src/utils/random_forest.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+from IPython.display import display
 import pandas as pd
 import numpy as np
 
@@ -13,21 +14,17 @@ import numpy as np
 # Custom transformer to split subjects
 class SubjectSplitter(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
-        # Extract unique subjects across all rows
         self.unique_subjects = set()
         for subjects in X:
             self.unique_subjects.update(subjects.split("$"))
         return self
 
     def transform(self, X):
-        # Create a DataFrame with binary columns for each unique subject
         subject_data = {subject: [] for subject in self.unique_subjects}
-
         for subjects in X:
             subject_set = set(subjects.split("$"))
             for subject in self.unique_subjects:
                 subject_data[subject].append(int(subject in subject_set))
-
         return pd.DataFrame(subject_data)
 
 
@@ -37,64 +34,60 @@ class AdaEmbeddingTransformer(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X):
-        # Truncate each embedding to the first 256 elements
         return np.array([embedding[:256] for embedding in X])
 
 
-# Load data
-data = pd.read_csv("data/data.csv")
+class MyModel:
+    def __init__(self):
+        self.text_transformer = Pipeline(steps=[("tfidf", TfidfVectorizer(stop_words="english"))])
+        self.subject_transformer = Pipeline(steps=[("splitter", SubjectSplitter())])
+        self.categorical_features = ["speaker_name", "speaker_job", "speaker_state", "speaker_affiliation", "statement_context"]
+        self.categorical_transformer = Pipeline(steps=[("onehot", OneHotEncoder(handle_unknown="ignore"))])
+        self.preprocessor = ColumnTransformer(
+            transformers=[
+                ("text", self.text_transformer, "statement"),
+                ("subject", self.subject_transformer, "subjects"),
+                ("cat", self.categorical_transformer, self.categorical_features),
+                # ("ada", AdaEmbeddingTransformer(), "ada_embedding"),
+            ]
+        )
+        self.model = Pipeline(
+            steps=[
+                ("preprocessor", self.preprocessor),
+                (
+                    "classifier",
+                    RandomForestClassifier(),
+                    # VotingClassifier(
+                    # estimators=[("rf", RandomForestClassifier(400)), ("gb", GradientBoostingClassifier(n_estimators=400))], voting="soft"
+                    # ),
+                ),
+            ]
+        )
 
-# Features and Labels
-X = data.drop(columns=["Label"])
-y = data["Label"].apply(lambda x: int("true" in x.lower()))
+    def train(self, df):
+        X = df.drop(columns=["Label"])
+        y = df["Label"].apply(lambda x: int("true" in x.lower()))
+        # X["ada_embedding"] = X["ada_embedding"].apply(eval)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Convert ADA embeddings from string to numerical list
-# X["ada_embedding"] = X["ada_embedding"].apply(eval)
+        self.model.fit(X_train, y_train)
 
-# Text Vectorization
-text_transformer = Pipeline(steps=[("tfidf", TfidfVectorizer(stop_words="english"))])
-
-# Categorical Encoding for subjects
-subject_transformer = Pipeline(steps=[("splitter", SubjectSplitter())])
-
-# Categorical Encoding for other features
-categorical_features = ["speaker_name", "speaker_job", "speaker_state", "speaker_affiliation", "statement_context"]
-categorical_transformer = Pipeline(steps=[("onehot", OneHotEncoder(handle_unknown="ignore"))])
-
-# Preprocessing Pipeline
-preprocessor = ColumnTransformer(
-    transformers=[
-        ("text", text_transformer, "statement"),
-        ("subject", subject_transformer, "subjects"),
-        ("cat", categorical_transformer, categorical_features),
-        # ("ada", AdaEmbeddingTransformer(), "ada_embedding"),
-    ]
-)
-
-# Model Pipeline
-model = Pipeline(
-    steps=[
-        ("preprocessor", preprocessor),
-        (
-            "classifier",
-            RandomForestClassifier(),
-            # VotingClassifier(
-            # estimators=[("rf", RandomForestClassifier(400)), ("gb", GradientBoostingClassifier(n_estimators=400))], voting="soft"
-            # ),
-        ),
-    ]
-)
-
-# Split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-
-# Train model
-print("training...")
-model.fit(X_train, y_train)
-
-# Predict and evaluate
-y_pred = model.predict(X_test)
-print(classification_report(y_test, y_pred))
-print(confusion_matrix(y_test, y_pred))
-print(matthews_corrcoef(y_test, y_pred))
+        y_pred = self.model.predict(X_test)
+        # Classification report
+        report = classification_report(y_test, y_pred, output_dict=True)
+        accuracy = report["accuracy"]
+        report_df = pd.DataFrame(report).transpose()
+        report_df = report_df.drop("accuracy")
+        display(report_df)
+        accuracy_df = pd.DataFrame({"accuracy": [accuracy]}).T
+        display(accuracy_df)
+        mcc = matthews_corrcoef(y_test, y_pred)
+        mcc_df = pd.DataFrame({"Matthews Correlation Coefficient": [mcc]})
+        display(mcc_df)
+        conf_matrix = confusion_matrix(y_test, y_pred)
+        conf_matrix_df = pd.DataFrame(
+            conf_matrix,
+            index=[f"Actual {i}" for i in range(conf_matrix.shape[0])],
+            columns=[f"Predicted {i}" for i in range(conf_matrix.shape[1])],
+        )
+        display(conf_matrix_df)
